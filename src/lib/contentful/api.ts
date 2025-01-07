2c1b0dfafd6e01babe3866933d27aa0a05f71ff4
Katarzyna <kasiabachur@gmail.com>
2025-01-07 23:40:59 +0100

    Initial commit

--- src/lib/contentful/api.ts
+++ src/lib/contentful/api.ts
@@ -1,3 +1,36 @@
+// import client from "./contentfulClient";
+
+// export async function allRecipes(kind: string | null) {
+//   const entry = await client.getEntries({
+//     content_type: "recipe",
+//     "fields.category": kind,
+//   });
+
+//   if (!entry) {
+//     return null;
+//   }
+
+//   const items = entry.items;
+
+//   console.log(items);
+
+//   return items;
+// }
+
+// export async function getRecipe(id: string) {
+//   const entry = await client.getEntries({
+//     content_type: "recipe",
+//     limit: 1,
+//     "fields.id": id,
+//   });
+
+//   if (!entry) {
+//     return null;
+//   }
+
+//   return entry.items[0].fields;
+// }
+
 import client from "./contentfulClient";
 
 export async function allRecipes(kind: string | null) {
@@ -30,8 +63,6 @@ export async function getRecipe(id: string) {
 
   return entry.items[0].fields;
 }
-
-
 // import client from "./contentfulClient";
 
 // export async function allRecipes(kind: string | null) {
