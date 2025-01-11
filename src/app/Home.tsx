ddb5c0df964ce35abad02781b0849c9c9253bf45
Katarzyna <kasiabachur@gmail.com>
2025-01-11 13:46:28 +0100

    fixed error

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -95,7 +95,7 @@ export default function Home({randomRecipes}: HomeProps) {
   </div>
 
   <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-    {randomRecipes.map((recipe) => {
+    {randomRecipes.map((recipe: any) => {
       const imageUrl = recipe.image;
 
       return (
