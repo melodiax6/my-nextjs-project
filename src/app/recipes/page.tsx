2be3e34dc6efcc6172853c7cb2c222c9a7d877ba
Katarzyna <kasiabachur@gmail.com>
2025-01-11 13:17:08 +0100

    fixing error

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,8 +1,12 @@
 
 import Recipes from './Recipes';
 
+type Params = {
+  params: {kind?: "meat"| "sweet"|"vege"}
+}
 
-export default async function Page({params}: {params: {kind?: "meat"| "sweet"|"vege"}}) {
+
+export default function Page({params}: Params) {
   return (
    <Recipes kind={params.kind ?? null} />
   );
